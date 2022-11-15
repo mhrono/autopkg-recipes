12321eff299bc4136436d96c678b762b0091e4fa
Matt Hrono <37225540+mhrono@users.noreply.github.com>
2022-11-15 11:12:54 -0500

    add AWSCLI jamf recipe

---
+++ Scripts/AWSCLIVersionEA.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ -e "/usr/local/aws/bin/aws" ]
+then
+	awsVersion=$(/usr/local/bin/aws --version 2>&amp;1 | /usr/bin/awk -F ' ' '/aws-cli/{ print $1}' | /usr/bin/awk -F / '{ print $2 }')
+else
+	awsVersion="Not Installed"
+fi
+
+echo "&lt;result&gt;$awsVersion&lt;/result&gt;"
