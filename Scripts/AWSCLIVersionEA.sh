54796b7b23923253897c9e22f0a29c416e72e8b7
Matt Hrono <37225540+mhrono@users.noreply.github.com>
2022-11-17 11:21:58 -0500

    Update AWSCLIVersionEA.sh

--- Scripts/AWSCLIVersionEA.sh
+++ Scripts/AWSCLIVersionEA.sh
@@ -2,9 +2,9 @@
 
 if [ -e "/usr/local/bin/aws" ]
 then
-	awsVersion=$(/usr/local/bin/aws --version 2>&amp;1 | /usr/bin/awk -F ' ' '/aws-cli/{ print $1}' | /usr/bin/awk -F / '{ print $2 }')
+	awsVersion=$(/usr/local/bin/aws --version | awk '{print $1}' | awk -F'/' '{print $2}')
 else
 	awsVersion="Not Installed"
 fi
 
-echo "&lt;result&gt;$awsVersion&lt;/result&gt;"
+echo "<result>$awsVersion</result>"
