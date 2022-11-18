83a4b718df44bde8a7cd459c02ec451639ee24df
Matt Hrono <37225540+mhrono@users.noreply.github.com>
2022-11-18 12:33:36 -0500

    recipe naming and smart group fixes

---
+++ Scripts/OpenJDKVersionEA.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+openjdkVersion="$(defaults read "${$(/usr/libexec/java_home)%/*}/Info" CFBundleShortVersionString 2>/dev/null || echo 'Not Installed')"
+
+echo "<result>$openjdkVersion</result>"
